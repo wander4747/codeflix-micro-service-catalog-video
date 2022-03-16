58d58266dc41d31ba057b1facbbc9f37cd5ca1c7
Wander Douglas <wander.douglas14@gmail.com>
2022-03-16 18:05:12 -0300

    value object (#2)

--- src/Core/Domain/Entity/Traits/MethodsMagicsTrait.php
+++ src/Core/Domain/Entity/Traits/MethodsMagicsTrait.php
@@ -18,4 +18,11 @@ trait MethodsMagicsTrait
         $className = get_class($this);
         throw new \Exception("Property {$property} not found in class {$className}");
     }
+
+
+    public function id(): string
+    {
+        return (string) $this->id;
+    }
+
 }
