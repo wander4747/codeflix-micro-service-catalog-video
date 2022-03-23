ff46c4c7b499ec385be228ecd0693c17b0d4782b
Wander Douglas <wander.douglas14@gmail.com>
2022-03-23 18:05:39 -0300

    create test/usecase update category (#7)

---
+++ src/Core/UseCase/Category/UpdateCategoryUseCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Core\UseCase\Category;
+
+use Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\UseCase\DTO\Category\UpdateCategory\{CategoryUpdateInputDto, CategoryUpdateOutputDto};
+
+class UpdateCategoryUseCase
+{
+    protected CategoryRepositoryInterface $repository;
+
+    public function __construct(CategoryRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function execute(CategoryUpdateInputDto $input): CategoryUpdateOutputDto
+    {
+        $category = $this->repository->findById($input->id);
+
+        $category->update(
+            name: $input->name,
+            description: $input->description ?? $category->description,
+        );
+
+        $categoryUpdated = $this->repository->update($category);
+
+        return new CategoryUpdateOutputDto(
+            id: $categoryUpdated->id,
+            name: $categoryUpdated->name,
+            description: $categoryUpdated->description,
+            isActive: $categoryUpdated->isActive,
+            created_at: $categoryUpdated->createdAt(),
+        );
+    }
+}
