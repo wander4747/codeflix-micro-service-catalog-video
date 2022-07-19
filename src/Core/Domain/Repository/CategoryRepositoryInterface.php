b8c36e80c332bf9fb8d769e002826202fda138ca
wander4747 <wander.douglas14@gmail.com>
2022-07-19 08:43:21 -0300

    create entity test

--- src/Core/Domain/Repository/CategoryRepositoryInterface.php
+++ src/Core/Domain/Repository/CategoryRepositoryInterface.php
@@ -7,14 +7,9 @@ use Core\Domain\Entity\Category;
 interface CategoryRepositoryInterface
 {
     public function insert(Category $entity): Category;
-
     public function findById(string $categoryId): Category;
-
     public function findAll(string $filter = '', $order = 'DESC'): array;
-
     public function paginate(string $filter = '', $order = 'DESC', int $page = 1, int $totalPage = 15): PaginationInterface;
-
     public function update(Category $category): Category;
-
     public function delete(string $categoryId): bool;
 }
