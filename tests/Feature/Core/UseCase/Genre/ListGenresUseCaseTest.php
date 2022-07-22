6ccb44cd2b32dc7c97e2622809e5aea44a1a1e3b
wander4747 <wander.douglas14@gmail.com>
2022-07-21 22:41:52 -0300

    create integration test usecase genre

---
+++ tests/Feature/Core/UseCase/Genre/ListGenresUseCaseTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Genre;
+
+use App\Models\Genre;
+use App\Repositories\Eloquent\GenreEloquentRepository;
+use Core\UseCase\DTO\Genre\List\ListGenresInputDto;
+use Core\UseCase\Genre\ListGenresUseCase;
+use Tests\TestCase;
+
+class ListGenresUseCaseTest extends TestCase
+{
+    public function testFindAll()
+    {
+        $useCase = new ListGenresUseCase(
+            new GenreEloquentRepository(new Genre())
+        );
+
+        Genre::factory()->count(100)->create();
+
+        $responseUseCase = $useCase->execute(
+            new ListGenresInputDto()
+        );
+
+        $this->assertEquals(15, count($responseUseCase->items));
+        $this->assertEquals(100, $responseUseCase->total);
+    }
+}
