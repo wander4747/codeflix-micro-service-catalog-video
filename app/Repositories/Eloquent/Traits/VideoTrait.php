ac7c78ab23047c61555dab3ab7d7413af0621021
wander4747 <wander.douglas14@gmail.com>
2022-08-08 09:30:03 -0300

    test repository insert video

---
+++ app/Repositories/Eloquent/Traits/VideoTrait.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Repositories\Eloquent\Traits;
+
+use App\Enums\ImageTypes;
+use App\Enums\MediaTypes;
+use Core\Domain\Entity\Video as Entity;;
+use Illuminate\Database\Eloquent\Model;
+
+trait VideoTrait
+{
+    public function updateMediaVideo(Entity $entity, Model $model): void
+    {
+        if ($mediaVideo = $entity->videoFile()) {
+            $action = $model->media()->first() ? 'update' : 'create';
+            $model->media()->{$action}([
+                'file_path' => $mediaVideo->filePath,
+                'media_status' => $mediaVideo->mediaStatus->value,
+                'encoded_path' => $mediaVideo->encodedPath,
+                'type' => MediaTypes::VIDEO->value,
+            ]);
+        }
+    }
+
+    public function updateMediaTrailer(Entity $entity, Model $model): void
+    {
+        if ($trailer = $entity->trailerFile()) {
+            $action = $model->trailer()->first() ? 'update' : 'create';
+            $model->trailer()->{$action}([
+                'file_path' => $trailer->filePath,
+                'media_status' => $trailer->mediaStatus->value,
+                'encoded_path' => $trailer->encodedPath,
+                'type' => MediaTypes::TRAILER->value,
+            ]);
+        }
+    }
+
+    public function updateImageBanner(Entity $entity, Model $model): void
+    {
+        if ($banner = $entity->bannerFile()) {
+            $action = $model->banner()->first() ? 'update' : 'create';
+            $model->banner()->{$action}([
+                'path' => $banner->path(),
+                'type' => ImageTypes::BANNER->value,
+            ]);
+        }
+    }
+
+    public function updateImageThumb(Entity $entity, Model $model): void
+    {
+        if ($thumb = $entity->thumbFile()) {
+            $action = $model->thumb()->first() ? 'update' : 'create';
+            $model->thumb()->{$action}([
+                'path' => $thumb->path(),
+                'type' => ImageTypes::THUMB->value,
+            ]);
+        }
+    }
+
+    public function updateImageThumbHalf(Entity $entity, Model $model): void
+    {
+        if ($thumbHalf = $entity->thumbHalf()) {
+            $action = $model->thumbHalf()->first() ? 'update' : 'create';
+            $model->thumbHalf()->{$action}([
+                'path' => $thumbHalf->path(),
+                'type' => ImageTypes::THUMB_HALF->value,
+            ]);
+        }
+    }
+}
---
+++ tests/Feature/App/Repositories/Eloquent/VideoEloquentRepositoryTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Feature\App\Repositories\Eloquent;
+
+use App\Enums\ImageTypes;
+use App\Models\{
+    Category,
+    CastMember,
+    Video as Model,
+    Genre,
+};
+use Core\Domain\Entity\Video as EntityVideo;
+use App\Repositories\Eloquent\VideoEloquentRepository;
+use Core\Domain\Enum\MediaStatus;
+use Core\Domain\Enum\Rating;
+use Core\Domain\Exception\NotFoundException;
+use Core\Domain\Repository\VideoRepositoryInterface;
+use Core\Domain\ValueObject\{
+    Image as ValueObjectImage,
+    Media as ValueObjectMedia,
+};
+use Core\Domain\ValueObject\Uuid;
+use DateTime;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class VideoEloquentRepositoryTest extends TestCase
+{
+    protected $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repository = new VideoEloquentRepository(
+            new Model()
+        );
+    }
+    
+    public function testImplementsInterface()
+    {
+        $this->assertInstanceOf(
+            VideoRepositoryInterface::class,
+            $this->repository
+        );
+    }
+
+    public function testInsert()
+    {
+        $entity = new EntityVideo(
+            title: 'Test',
+            description: 'Test',
+            yearLaunched: 2026,
+            rating: Rating::L,
+            duration: 1,
+            opened: true,
+        );
+
+        $this->repository->insert($entity);
+
+        $this->assertDatabaseHas('videos', [
+            'id' => $entity->id(),
+        ]);
+    }
+
+    public function testInsertWithRelationships()
+    {
+        $categories = Category::factory()->count(4)->create();
+        $genres = Genre::factory()->count(4)->create();
+        $castMembers = CastMember::factory()->count(4)->create();
+
+        $entity = new EntityVideo(
+            title: 'Test',
+            description: 'Test',
+            yearLaunched: 2026,
+            rating: Rating::L,
+            duration: 1,
+            opened: true,
+        );
+        foreach ($categories as $category) {
+            $entity->addCategory($category->id);
+        }
+        foreach ($genres as $genre) {
+            $entity->addGenre($genre->id);
+        }
+        foreach ($castMembers as $castMember) {
+            $entity->addCastMember($castMember->id);
+        }
+
+        $entityInDb = $this->repository->insert($entity);
+
+        $this->assertDatabaseHas('videos', [
+            'id' => $entity->id(),
+        ]);
+
+        $this->assertDatabaseCount('category_video', 4);
+        $this->assertDatabaseCount('genre_video', 4);
+        $this->assertDatabaseCount('cast_member_video', 4);
+
+        $this->assertEquals($categories->pluck('id')->toArray(), $entityInDb->categoriesId);
+        $this->assertEquals($genres->pluck('id')->toArray(), $entityInDb->genresId);
+        $this->assertEquals($castMembers->pluck('id')->toArray(), $entityInDb->castMembersId);
+    }
+}
